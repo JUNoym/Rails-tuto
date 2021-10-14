ec5cee023ae8771df5112f25e2e96c412f9322e3
JUNoym <junnyaooyama@gmail.com>
2021-10-15 04:31:27 +0900

    ログインしてユーザー画面に遷移成功 loginのヘルパーメソッド作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # 入力されたメールアドレスを持つユーザーがデータベースに存在し、かつ入力されたパスワードがそのユーザーのパスワードである場合↑
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user # ヘルパーメソッド　ユーザーのブラウザ内の一時cookiesに暗号化済みのユーザーIDが自動で作成されます
+      redirect_to user
     else
-      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    # 上のコードを実行すると、ユーザーのブラウザ内の一時cookiesに暗号化済みのユーザーIDが自動で作成されます
 end
