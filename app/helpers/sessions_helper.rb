687ad89504529fe908960850a9a02f5a3439927b
JUNoym <junnyaooyama@gmail.com>
2021-10-16 08:50:03 +0900

    ログインの状態に応じてヘッダーのリンク切り替えを実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,15 @@ module SessionsHelper
         session[:user_id] = user.id
     end
     # 上のコードを実行すると、ユーザーのブラウザ内の一時cookiesに暗号化済みのユーザーIDが自動で作成されます
+    def current_user
+        if session[:user_id]
+            @current_user = @current_user || User.find_by(id: session[:user_id])        
+            # @current_user ||= User.find_by(id: session[:user_id]) ← このようにも書くことができる
+        end
+    end
+
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+    def logged_in?
+        !current_user.nil?
+    end
 end
